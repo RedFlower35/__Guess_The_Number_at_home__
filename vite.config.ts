7de88649f3bec33dc50b8eabb9b05a5932dbfbfa
Jimmy Wang <wangcbstudy@gmail.com>
2026-05-14 12:31:40 +0800

    修改 vite.config.ts_讓我可以部署

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig({
+  base: "/__Guess_The_Number_at_home__/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
